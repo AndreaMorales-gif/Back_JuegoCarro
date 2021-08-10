d3140069d9f687a053dd512d739e7755f43d3af0
Andrea Morales <andmorales11@ielasnieves.edu.co>
2021-08-10 18:51:41 -0500

    Se creo el rograma base de la apliación en SpringInicializart.

---
+++ src/main/java/com/co/Juego/JuegoCarro/JuegoCarroApplication.java
@@ -0,0 +1,13 @@
+package com.co.Juego.JuegoCarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/Juego/JuegoCarro/JuegoCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.Juego.JuegoCarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
